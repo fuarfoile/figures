028b003e5f3da446e9248f712069a207b0545c26
Oleh Bondarenko <boa.st@ya.ru>
2021-02-09 20:46:00 +0200

    Update FiguresApplication.java

--- src/main/java/com/boast/FiguresApplication.java
+++ src/main/java/com/boast/FiguresApplication.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+//Main class
 public class FiguresApplication {
     public static void main(String[] args) {
         Random random = new Random();
@@ -11,10 +12,12 @@ public class FiguresApplication {
         List<Figure> figures = new ArrayList<Figure>();
         int arrayLen = random.nextInt(10) + 1;
 
+        //adding random number (arrayLen) of random figures to the array
         for(int i = 0; i < arrayLen; i++){
             figures.add(RandomFigure.getRandomFigure());
         }
 
+        //Printing all figures from the array
         System.out.println("Array length: " + arrayLen);
         for (Figure figure : figures) {
             System.out.println(figure);
